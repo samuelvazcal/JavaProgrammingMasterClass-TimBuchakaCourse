971b46977d50e81cc54a97165b8b9831439a8047
Samuel Vazquez <samuel_vazquez@epam.com>
2020-07-22 00:03:13 -0500

    Added exercise for floating point precision

---
+++ PoundsKilograms-Challenge/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		double numberOfPounds = 200;
+		double convertedKilograms = numberOfPounds*0.45359;
+		System.out.println("Converted kilograms: " + convertedKilograms);
+	}
+}
