1905ce09cccfd049bc4e624237b79a06e428c628
Samuel Vazquez <samuel_vazquez@epam.com>
2020-07-29 13:44:04 -0500

    Exercice about method overloading in length converter

--- MethodOverloading/src/Main.java
+++ MethodOverloading/src/Main.java
@@ -5,8 +5,55 @@ public class Main {
 		calculateScore(75);
 		calculateScore();
 
+		System.out.println("\nChallenge!: \n");
+		System.out.println(calcFeetAndInchesToCentimeters(1,0));
+		System.out.println(calcFeetAndInchesToCentimeters(2,8));
+		System.out.println(calcFeetAndInchesToCentimeters(0,7));
+		System.out.println(calcFeetAndInchesToCentimeters(3,2));
+		System.out.println(calcFeetAndInchesToCentimeters(-1,-1));
+		System.out.println(calcFeetAndInchesToCentimeters(-2,2));
+
+
+
+
+
+
+		//Create a method called calcFeetAndInchesToCentimeters
+		//It needs to gave two parameters.
+		//feet is the first parameter, inches is the 2nd parameter
+		//
+		//You should validate that the first parameter feet is >=0
+		//You should validate that the 2nd parameter inches is >=0 and <=12
+		//return .1 from the method if either of the above is not true
+		//
+		//If the parameters are valid, then calculate how many centimeters
+		//comprise the feet and inches passed to this method and return that value.
+		//
+		//Create a 2nd method of the same name with only one parameter
+		//inches is the parameter
+		//validate that its >=0
+		//return -1 if it is not true
+		//But if its valid, then calculate how many feet are in the inches
+		//and then here is the tricky part
+		//call the other overloaded method passing the correct feet and inches
+		//hints: Use double for your number datatypes is probably a good idea
+		//1 inch = 2.54 cm. and one foot = 12 inches
+
 
 	}
+
+	public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+		if((feet >= 0) && (inches >= 0 && inches <= 12)) {
+			return ((feet*12*2.54) + calcFeetAndInchesToCentimeters(inches));
+		} else return -1;
+	}
+
+	public static double calcFeetAndInchesToCentimeters(double inches) {
+		if(inches >= 0 && inches <= 12) {
+			return inches*2.54;
+		} return -1;
+	}
+
 	public static int calculateScore(String playerName, int score) {
 		System.out.println("Player " + playerName + " scored " + score + " points");
 		return score * 1000;
