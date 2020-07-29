53d977f4aec6ea1411d2b6050e3e177f303ff774
Samuel Vazquez <samuel_vazquez@epam.com>
2020-07-29 12:37:35 -0500

    Exercise to return a boolean value after a math operation

---
+++ CodingExercise6-EqualSumChecker/src/EqualSumChecker.java
@@ -0,0 +1,7 @@
+public class EqualSumChecker {
+	public static boolean hasEqualSum (int param1, int param2, int param3) {
+		if((param1+param2) ==param3){
+			return true;
+		} else return false;
+	}
+}
