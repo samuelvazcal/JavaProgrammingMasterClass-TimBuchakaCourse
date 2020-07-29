76522111d88d0eec2f27ab7808771f2712e9c543
Samuel Vazquez <samuel_vazquez@epam.com>
2020-07-29 14:43:16 -0500

    Overloaded methods

---
+++ SecondsAndMinutes-Challenge/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("First overloaded method:");
+		getDurationString(80,32);
+		getDurationString(64,90);
+		getDurationString(124,10);
+		System.out.println("Second overloaded method:");
+		getDurationString(3264);
+		getDurationString(216000);
+		getDurationString(61);
+		getDurationString(6227);
+		getDurationString(-8);
+
+	}
+
+	public static void getDurationString(int minutes, int seconds) {
+		if((minutes >= 0) && (seconds >= 0 && seconds <= 59)) {
+			int hours = minutes / 60;
+			int remainingMinutes = minutes % 60;
+			System.out.println(hours + "h " + remainingMinutes + "m " + seconds + "s");
+		} else
+			System.out.println("Invalid Value");
+	}
+	public static void getDurationString(int seconds) {
+		if(seconds >= 0) {
+			int minutes = seconds / 60;
+			int remainingSeconds = seconds % 60;
+			getDurationString(minutes,remainingSeconds);
+		} else
+		System.out.println("Invalid Value");
+	}
+}
