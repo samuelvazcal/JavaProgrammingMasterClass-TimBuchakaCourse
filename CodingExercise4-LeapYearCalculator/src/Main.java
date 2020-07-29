fca4ecb3729ab8f8e5abfac18cea87726281cc31
Samuel Vazquez <samuel_vazquez@epam.com>
2020-07-29 11:50:59 -0500

    Excersise to find out if a year is a leap year

---
+++ CodingExercise4-LeapYearCalculator/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(LeapYear.isLeapYear(-1600));
+		System.out.println(LeapYear.isLeapYear(1600));
+		System.out.println(LeapYear.isLeapYear(2017));
+		System.out.println(LeapYear.isLeapYear(2000));
+		System.out.println(LeapYear.isLeapYear(1924));
+		System.out.println(LeapYear.isLeapYear(1800));
+
+	}
+}
