2657500293548234db3ba8cee19b05f2f5241c07
Samuel Vazquez <samuel_vazquez@epam.com>
2020-07-30 17:07:10 -0500

    Added exercise to find prime numbers

--- ForStatement-Challenge/src/Main.java
+++ ForStatement-Challenge/src/Main.java
@@ -14,8 +14,47 @@ public class Main {
 			double result = calculateInterest(10000,i);
 			System.out.println("10000 at " + i + "% interest = " + String.format("%.2f",result));
 		}
+
+		System.out.println("\nChallenge 2!: \n");
+		//How would you modify the foor loop above to do the same thing as
+		//shown but to start from 8% and work back to 2%
+
+		for(int i=8; i >= 2; i--) {
+			double result = calculateInterest(10000,i);
+			System.out.println("10000 at " + i + "% interest = " + String.format("%.2f",result));
+		}
+
+		int count = 0;
+		for(int i =1; i<50; i++) {
+			if(isPrime(i)) {
+				count++;
+				System.out.println("Number " + i + " is a prime number");
+				if(count == 15) {
+					System.out.println("Exiting for loop");
+					break;
+				}
+			}
+		}
 	}
 
+
+	//Create a for statement using any range of numbers
+	//Determine if the number is a prime number using the isPrime method
+	//if it is a prime number, print it out, AND increment a count of the
+	//number of prime numbers found
+	//if that count is 3 exit the foor loop
+	//hint: Use the break; statement to exit.
+	public static boolean isPrime(int n) {
+		if(n==1) {
+			return false;
+		}
+		for(int i=2; i<=n/2; i++) {
+			if(n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 
 	public static double calculateInterest(double amount, double interestRate) {
