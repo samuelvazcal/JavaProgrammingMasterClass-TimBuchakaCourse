c1c669efb71be996faf5bce9775701c324e1559f
Samuel Vazquez <samuel_vazquez@epam.com>
2020-07-30 10:08:38 -0500

    Exercise using switch statement

---
+++ CodingExercise11-NumberInWord/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		NumberInWord.printNumberInWord(0);
+		NumberInWord.printNumberInWord(1);
+		NumberInWord.printNumberInWord(2);
+		NumberInWord.printNumberInWord(3);
+		NumberInWord.printNumberInWord(10);
+	}
+}
