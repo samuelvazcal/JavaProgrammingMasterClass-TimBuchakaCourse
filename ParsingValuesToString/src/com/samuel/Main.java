6abb5c8b46d2a894b3bc11aaada1ae868c8704f3
Samuel Vazquez <samuel_vazquez@epam.com>
2020-08-14 11:49:38 -0500

    Using different parse methods

--- ParsingValuesToString/src/com/samuel/Main.java
+++ ParsingValuesToString/src/com/samuel/Main.java
@@ -10,5 +10,14 @@ public class Main {
 		number += 1;
 		System.out.println("numberAsString = " +numberAsString);
 		System.out.println("number = " + number);
+
+		String numberAsStringA = "2018.321";
+		double numberA = Double.parseDouble(numberAsStringA);
+		System.out.println("numberA = " + numberA);
+		numberAsStringA += 1;
+		numberA += 1;
+		System.out.println("numberAsString = " +numberAsStringA);
+		System.out.println("numberA = " + numberA);
+
 	}
 }
