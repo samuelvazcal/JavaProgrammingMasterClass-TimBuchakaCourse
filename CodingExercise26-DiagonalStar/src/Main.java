35440b427d2984b748b8cc7d52cc4d0da1580e43
Samuel Vazquez <samuel_vazquez@epam.com>
2020-08-14 18:10:35 -0500

    Exercise to print an array of *

---
+++ CodingExercise26-DiagonalStar/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		DiagonalStar.printSquareStar(5);
+		System.out.println("\n");
+		DiagonalStar.printSquareStar(3);
+		System.out.println("\n");
+		DiagonalStar.printSquareStar(7);
+
+	}
+}
