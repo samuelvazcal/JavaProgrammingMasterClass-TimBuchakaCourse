e912caa248e586fda33f5fa2c964d6c61d8bb993
Samuel Vazquez <samuel_vazquez@epam.com>
2020-09-21 16:44:40 -0500

    Find distance between two points using overloaded methods

---
+++ CodingExercise32-Point/src/Point.java
@@ -0,0 +1,41 @@
+public class Point {
+	private int x;
+	private int y;
+
+	public Point() {
+
+	}
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public double distance() {
+		return Math.sqrt(((getX())*(getX()))+(((getY())*(getY()))));
+	}
+
+	public double distance(int x, int y) {
+		return Math.sqrt(((x-getX())*(x-getX())+((y-getY())*(y-getY()))));
+	}
+
+	public double distance(Point another) {
+		return distance(another.getX(),another.getY());
+	}
+}
