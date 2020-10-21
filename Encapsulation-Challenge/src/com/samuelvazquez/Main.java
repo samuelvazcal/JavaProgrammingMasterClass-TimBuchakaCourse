8383721f485190fc9407b87aaa7dc64bd61c9f0e
Samuel Vazquez <samuel_vazquez@epam.com>
2020-10-21 16:13:49 -0500

    Test for getPagesPrinted

--- Encapsulation-Challenge/src/com/samuelvazquez/Main.java
+++ Encapsulation-Challenge/src/com/samuelvazquez/Main.java
@@ -2,7 +2,7 @@ package com.samuelvazquez;
 
 public class Main {
 	public static void main(String[] args) {
-		Printer printer = new Printer(50,false);
+		Printer printer = new Printer(50,true);
 		System.out.println("Initial page count: " + printer.getPagesPrinted());
 		int pagesPrinted = printer.printPages(4);
 		System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
