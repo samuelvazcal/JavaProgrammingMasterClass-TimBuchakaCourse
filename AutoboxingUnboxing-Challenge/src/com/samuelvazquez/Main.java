7ecddcd5181336bd491d84f4552b12bd658fc067
Samuel Vazquez <samuel_vazquez@epam.com>
2020-11-27 10:29:31 -0600

    Extra testing for bank methods situations

--- AutoboxingUnboxing-Challenge/src/com/samuelvazquez/Main.java
+++ AutoboxingUnboxing-Challenge/src/com/samuelvazquez/Main.java
@@ -16,7 +16,15 @@ public class Main {
 		bank.addCustomerTransaction("Adelaide","Tim",12.44);
 		bank.addCustomerTransaction("Adelaide","Mike",1.44);
 
-		bank.listCustomer("Adelaide",false);
+		bank.listCustomer("Adelaide",true);
+		bank.listCustomer("Sydney",true);
+
+		bank.addBranch("Melbourne");
+
+		if(!bank.addCustomer("Melbourne","Brian",5.53)) {
+			System.out.println("Error, Melbourne branch does not exists");
+		}
+
 
 	}
 
