d253f3850ab270fc327fd2d2d93d22c7dfb42d2f
Samuel Vazquez <samuel_vazquez@epam.com>
2020-12-14 14:20:56 -0600

    Interfaces challenge -  objects declaration

---
+++ Interfaces-Challenge/src/com/samuelvazquez/ISaveable.java
@@ -0,0 +1,13 @@
+package com.samuelvazquez;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public interface ISaveable {
+
+	List<String> write();
+	void read(List<String> savedValues);
+
+
+}
