6a92a9689211644a72a1e99c131d93cc937ee259
Samuel Vazquez <samuel_vazquez@epam.com>
2021-01-12 17:21:43 -0600

    Set and HashSet in Java Collections

---
+++ SetAndHashSet/src/com/samuelvazquez/HeavenlyBody.java
@@ -0,0 +1,37 @@
+package com.samuelvazquez;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class HeavenlyBody {
+	private final String name;
+	private final double orbitalPeriod;
+	private final Set<HeavenlyBody> satellites;
+
+	public HeavenlyBody(String name, double orbitalPeriod) {
+		this.name = name;
+		this.orbitalPeriod = orbitalPeriod;
+		this.satellites = new HashSet<>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getOrbitalPeriod() {
+		return orbitalPeriod;
+	}
+
+	//So we want to have the option to add a moon
+	public boolean addMoon(HeavenlyBody moon) {
+		return this.satellites.add(moon);
+	}
+
+	//just for security reasons about immutable classes
+	// we are creating a new hash set of the current contents of our satellite which again means
+	//that the code that is calling this object doesn't have access to our original hash set
+	public Set<HeavenlyBody> getSatellites() {
+		return new HashSet<>(this.satellites);
+	}
+}
