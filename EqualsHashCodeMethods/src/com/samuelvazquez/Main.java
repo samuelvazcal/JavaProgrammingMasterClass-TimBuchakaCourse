4c6235bda89b6012ec3406aa228472cedc24337e
Samuel Vazquez <samuel_vazquez@epam.com>
2021-01-13 13:27:33 -0600

    Importance of 'equals' method overriding

--- EqualsHashCodeMethods/src/com/samuelvazquez/Main.java
+++ EqualsHashCodeMethods/src/com/samuelvazquez/Main.java
@@ -10,12 +10,19 @@ public class Main {
 		Client client2 = new Client("Rodolfo Neri Vela","0002",1800);
 		Client client3 = new Client("Paola Espinoza","0003",1900);
 		Client client4 = new Client("Emilia Calvario","0004", 2100);
+		Client client5 = new Client("Guillermo del Toro", "0001",2000);
 
+		//Set is an unordered collection of objects in which duplicate values cannot be stored.
+		//But we have to overwrite the equals and hashCode methods first. If not, we will collect
+		//duplicate values even when we shouldn't
+		//Have a look in line 13
 		Set<Client> clientsSet = new HashSet<Client>();
 		clientsSet.add(client1);
 		clientsSet.add(client2);
 		clientsSet.add(client3);
 		clientsSet.add(client4);
+		//Once we ad the client5 to the ClientsSet without overriding equals method, we can save a duplicate in our collection
+		clientsSet.add(client5);
 
 		for(Client client: clientsSet) {
 			System.out.println("Name: " + client.getName() + "| id: " + client.getId() + "| credit: " + client.getCredit());
