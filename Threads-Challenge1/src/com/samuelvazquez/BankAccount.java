ecc410c671da4b776f5122ee60d8f33a8576fa5a
Samuel Vazquez <samuel_vazquez@epam.com>
2021-02-23 14:07:31 -0600

    Challenge 4 - Using ReentrantLock instances

--- Threads-Challenge1/src/com/samuelvazquez/BankAccount.java
+++ Threads-Challenge1/src/com/samuelvazquez/BankAccount.java
@@ -1,13 +1,21 @@
 package com.samuelvazquez;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
 class BankAccount {
 
 	private double balance;
 	private String accountNumber;
 
+	private Lock lock;
+
 	public BankAccount(String accountNumber, double initialBalance) {
 		this.accountNumber = accountNumber;
 		this.balance = initialBalance;
+		//it will be initialize in te constructor
+		this.lock = new ReentrantLock();
 	}
 	//Making BankAccount threadsafe, one solution is synchronizing any method that is updating a field
 
@@ -26,17 +34,17 @@ class BankAccount {
 
 	//Generally speaking we want to synchronize the smallest amount of code possible to minimize the
 	//performance impact on the application
-	public void deposit(double amount) {
-		synchronized (this) {
-			balance += amount;
-		}
-	}
-
-	public void withdraw(double amount) {
-		synchronized (this) {
-			balance -= amount;
-		}
-	}
+//	public void deposit(double amount) {
+//		synchronized (this) {
+//			balance += amount;
+//		}
+//	}
+//
+//	public void withdraw(double amount) {
+//		synchronized (this) {
+//			balance -= amount;
+//		}
+//	}
 
 	public String getAccountNumber() {
 		return accountNumber;
@@ -49,4 +57,32 @@ class BankAccount {
 	//Make the BnkAccount class threadsafe again using the synchronized keyword
 	//We don't have to make any changes to the code. Because both threads only read the account number,
 	//thread interference isn't an issue. We'b be over-synchronizing the code
+
+	//Use ReentrantLock
+	//Instead of suing the synchronized keyword, make the BankAccount class threadsafe using
+	//the ReentrantLock class
+	//We'll create a lock object and use the lock() method in the withdraw() and deposit() methods.
+	// - Both threads need to compete for the same lock, so we create only one lock object that we
+	// use in all the methods that have critical sections of code
+	// - We put the critical section of code within a try/finally block, to ensure that the lock will
+	//be released.
+	//Don't forget to create the lock instance in the BankAccount constructor.
+
+	public void deposit(double amount) {
+		lock.lock();
+		try {
+			balance += amount;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public void withdraw(double amount) {
+		lock.lock();
+		try {
+			balance -= amount;
+		} finally {
+			lock.unlock();
+		}
+	}
 }
