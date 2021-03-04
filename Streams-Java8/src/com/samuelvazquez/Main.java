f13805d146a04b86b519dc621ee247dbfb67e03f
Samuel Vazquez <samuel_vazquez@epam.com>
2021-03-04 09:55:54 -0600

    Lambda implementation

---
+++ Streams-Java8/src/com/samuelvazquez/Main.java
@@ -0,0 +1,29 @@
+package com.samuelvazquez;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> someBingoNumbers = Arrays.asList(
+              "N40","N36",
+              "B12","B6",
+              "G53","G49", "G60", "G50", "g64",
+              "I26","I17","I29","071");
+
+        List<String> gNumbers = new ArrayList<>();
+        someBingoNumbers.forEach(number->{
+            if(number.toUpperCase().startsWith("G")) {
+                gNumbers.add(number);
+                //System.out.println(number);
+            }
+        });
+
+        gNumbers.sort((String s1, String s2) -> s1.compareTo(s2));
+        gNumbers.forEach((String s)-> System.out.println(s));
+    }
+}
