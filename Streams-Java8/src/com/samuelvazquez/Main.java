fa6528afe88897683e878322893471d18794b3f8
Samuel Vazquez <samuel_vazquez@epam.com>
2021-03-04 10:37:40 -0600

    Stream implementation

--- Streams-Java8/src/com/samuelvazquez/Main.java
+++ Streams-Java8/src/com/samuelvazquez/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 
 public class Main {
@@ -13,17 +14,31 @@ public class Main {
               "N40","N36",
               "B12","B6",
               "G53","G49", "G60", "G50", "g64",
-              "I26","I17","I29","071");
+              "I26","I17","I29","O71");
 
-        List<String> gNumbers = new ArrayList<>();
-        someBingoNumbers.forEach(number->{
-            if(number.toUpperCase().startsWith("G")) {
-                gNumbers.add(number);
-                //System.out.println(number);
-            }
-        });
+//        List<String> gNumbers = new ArrayList<>();
+        ////        someBingoNumbers.forEach(number->{
+        ////            if(number.toUpperCase().startsWith("G")) {
+        ////                gNumbers.add(number);
+        ////                //System.out.println(number);
+        ////            }
+        ////        });
+        ////
+        ////        gNumbers.sort((String s1, String s2) -> s1.compareTo(s2));
+        ////        gNumbers.forEach((String s)-> System.out.println(s));
+        someBingoNumbers
+              .stream()
+              .map(String::toUpperCase)
+              //.filter(s->s.startsWith("G"))
+              .filter(s -> s.startsWith("G"))
+              .sorted()
+              .forEach(System.out::println);
 
-        gNumbers.sort((String s1, String s2) -> s1.compareTo(s2));
-        gNumbers.forEach((String s)-> System.out.println(s));
+        Stream<String>ioNumberStream = Stream.of("I26","I17","I29","071");
+        Stream<String>inNumberStream = Stream.of("N40","N36","I26","I17","I29","O71");
+        Stream<String>concatStream = Stream.concat(ioNumberStream, inNumberStream);
+        System.out.println("-----------");
+        System.out.println(concatStream.distinct().peek(System.out::println).count());
+
     }
 }
